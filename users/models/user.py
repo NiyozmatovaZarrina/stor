50f8b7597aac35bd4a5aa293b860d930c3d04393
NiyozmatovaZarrina <zarrinakom@mail.ru>
2023-08-10 22:46:13 +0500

    user

---
+++ users/models/user.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username=models.CharField("Название", max_length=100)
+    email=models.EmailField("Email", max_length=254)
+    password= models.IntegerField("Пароль")    
+    
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+         
+    
+    def __str__(self) -> str:
+        return self.username
+
