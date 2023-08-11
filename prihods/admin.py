b780f4b67e2a811f457825437d14195f55f34fc9
NiyozmatovaZarrina <zarrinakom@mail.ru>
2023-08-11 22:37:20 +0500

    ptihod

--- prihods/admin.py
+++ prihods/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Prihod
 
 # Register your models here.
+
+@admin.register(Prihod)
+class PrihodAdmin(admin.ModelAdmin):
+    list_display =["user","totalprice"]
+   # list_filter=[" totalprice"]
+
+
+
